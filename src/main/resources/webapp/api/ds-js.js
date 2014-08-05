ae58601c52b8b8fea1832bf5085f8bc83f3e61fb
Filipe Carmona Simões <filipe.csimoes@gmail.com>
2014-08-05 17:08:18 -0300

    api js

--- src/main/resources/webapp/api/ds-js.js
+++ src/main/resources/webapp/api/ds-js.js
@@ -2,12 +2,17 @@ define(['jquery'], function($) {
 	function Query(kind) {
 
 		var EQ_FILTER = '=';
+		var NEQ_FILTER = '!=';
 		var GT_FILTER = '>';
+		var GTE_FILTER = '>=';
+		var LTE_FILTER = '<=';
 		var LT_FILTER = '<';
+		var IN_FILTER = 'in';
 
 		var _this = this;
 		this.kind = kind;
 		this.filters = [];
+		this.order = [];
 		this.select = function() {
 			this.fields = $.makeArray(arguments);
 			return this;
@@ -15,15 +20,33 @@ define(['jquery'], function($) {
 		this.eq = function(field, value) {
 			return addFilter(field, EQ_FILTER, value);
 		}
+		this.inEq = function() {
+			var field = arguments[0];
+			var value = $.makeArray(arguments).slice(1);
+			return addFilter(field, IN_FILTER, value);
+		}
+		this.neq = function(field, value) {
+			return addFilter(field, NEQ_FILTER, value);
+		}
 		this.gt = function(field, value) {
 			return addFilter(field, GT_FILTER, value);
+		}
+		this.gte = function(field, value) {
+			return addFilter(field, GTE_FILTER, value);
 		}
 		this.lt = function(field, value) {
 			return addFilter(field, LT_FILTER, value);
 		}
+		this.lte = function(field, value) {
+			return addFilter(field, LTE_FILTER, value);
+		}
 		var addFilter = function(field, operator, value) {
 			_this.filters.push({field: field, operator: operator, value: value});
 			return _this;
+		}
+		this.order = function() {
+			this.order = $.makeArray(arguments);
+			return this;
 		}
 		this.toJSON = function() {
 			var json = {};
@@ -31,6 +54,9 @@ define(['jquery'], function($) {
 			toSafeJSON('fields', this.fields, json);
 			if (this.filters.length > 0) {
 				json.filters = this.filters;
+			}
+			if (this.order.length > 0) {
+				json.order = this.order;
 			}
 			return json;
 		}
