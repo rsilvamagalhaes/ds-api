8bc0facce988f7bb6dbbd55c465a184ed58622c8
felipe volpone <felipevolpone@gmail.com>
2014-08-07 11:34:13 -0300

    Refatorando: get, iteracao em cima dos models, toJSON. Retornando json esperado

--- query.py
+++ query.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb
 from bottle import Bottle
 import sys
+
 bottle = Bottle()
 
 
@@ -17,15 +18,20 @@ def create_generic_model(kind):
 def toJSON(query_result):
     list = []
     for model in query_result:
-        item = {}
-        for property in model._properties.keys():
-            value = getattr(model, property)
-            item[property] = value
-
+        item = model_to_json(model)
         list.append(item)
 
-    return {'result': list}
+    return list
 
+
+def model_to_json(model):
+    item = {}
+    for property in model._properties.keys():
+        value = getattr(model, property)
+        item['field'] = property
+        item['value'] = value
+        item['type'] = value.__class__.__name__
+    return item
 
 @bottle.get('/query/put')
 def put():
@@ -69,22 +75,48 @@ def get():
         'limit': 1
     }
 
+    #json = {
+    #    'kind': 'User',
+    #    'id': 4785074604081152
+    #}
 
-    user = create_generic_model(json['kind'])
-    query = user.query()
-    if 'filters' in json:
-        for filter in json['filters']:
-            query = do_query_based_on_operator(filter, query)
+    kind = json['kind']
+    user = create_generic_model(kind)
+
+    json_result = {}
+
+    #key methods
+    if 'name' in json or 'id' in json:
+        json_result = get_results_from_key(kind, json)
+
+    else:
+        #query methods
+        query = user.query()
+
+        if 'filters' in json:
+            for filter in json['filters']:
+                query = do_query_based_on_operator(filter, query)
+
+        if 'order' in json:
+            query = order_query(json['order'], query)
+
+        limit = None
+        if 'limit' in json:
+            limit = int(json['limit'])
+
+        json_result = toJSON(query.fetch(limit))
+
+    return {'result': [json_result]}
 
 
-    if 'order' in json:
-        query = order_query(json['order'], query)
+def get_results_from_key(kind, json):
+    if 'name' in json:
+        key = ndb.Key(kind, json['name'])
 
-    limit = None
-    if 'limit' in json:
-        limit = int(json['limit'])
+    if 'id' in json:
+        key = ndb.Key(kind, json['id'])
 
-    return toJSON(query.fetch(limit))
+    return model_to_json(key.get())
 
 
 def order_query(order_json, query):
