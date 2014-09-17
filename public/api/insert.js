4f6479924a25dabf4c25330466001f6795c08495
felipevolpone <felipevolpone@gmail.com>
2014-09-17 17:08:13 -0300

    Tela de inserir nova entidade

---
+++ public/api/insert.js
@@ -0,0 +1,15 @@
+define(['jquery'], function($, ds) {
+
+	function InsertView() {
+
+		
+		
+	}
+
+	var init = function() {
+		var view = new InsertView();
+		return view;
+	}
+
+	return {init : init};
+});
